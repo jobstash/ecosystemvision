b763dd5e09754152787ef7627a84608e92624178
John Ballesteros <johnshift.dev@gmail.com>
2024-05-15 11:26:27 +0800

    feat: add filters-section component

---
+++ src/filters/components/fileters-section/index.tsx
@@ -0,0 +1,24 @@
+import { PrimitiveAtom } from 'jotai';
+
+import { FilterConfigMapper } from '@/filters/components/filter-config-mapper';
+import { FilterQueryInput } from '@/filters/components/filter-query-input';
+import { FilterToggler } from '@/filters/components/filter-toggler';
+
+import { TotalCount } from './total-count';
+
+interface Props {
+  countAtom: PrimitiveAtom<number | null>;
+  searchPlaceholder: string;
+}
+
+export const FiltersSection = ({ countAtom, searchPlaceholder }: Props) => {
+  return (
+    <div className="flex flex-col gap-4">
+      <FilterQueryInput placeholder={searchPlaceholder} />
+
+      <FilterToggler countSection={<TotalCount countAtom={countAtom} />}>
+        <FilterConfigMapper />
+      </FilterToggler>
+    </div>
+  );
+};
