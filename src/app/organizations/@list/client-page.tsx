0168edb3c1a72b0d740b28610438bdf72d236864
John Ballesteros <johnshift.dev@gmail.com>
2024-05-15 11:56:42 +0800

    feat(app): add org-list page

---
+++ src/app/organizations/@list/client-page.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+
+import { ROUTE_SECTIONS } from '@/shared/core/constants';
+
+import { orgFiltersSearchParamsAtom } from '@/filters/core/atoms';
+import { orgTotalCountAtom } from '@/orgs/core/atoms';
+import { FiltersProvider } from '@/filters/providers/filters-provider';
+
+const FiltersSection = dynamic(() =>
+  import('@/filters/components/filters-section').then((m) => m.FiltersSection),
+);
+
+const OrgList = dynamic(() =>
+  import('@/orgs/components/org-list').then((m) => m.OrgList),
+);
+
+interface Props {
+  rawSearchParams: Record<string, string>;
+}
+
+export const OrgListClientPage = ({ rawSearchParams }: Props) => {
+  return (
+    <FiltersProvider
+      rawSearchParams={rawSearchParams}
+      routeSection={ROUTE_SECTIONS.ORGS}
+      atom={orgFiltersSearchParamsAtom}
+    >
+      <FiltersSection
+        countAtom={orgTotalCountAtom}
+        searchPlaceholder="Search organizations ..."
+      />
+      <OrgList />
+    </FiltersProvider>
+  );
+};
---
+++ src/app/organizations/@list/page.tsx
@@ -0,0 +1,57 @@
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+
+import { ROUTE_SECTIONS } from '@/shared/core/constants';
+import { getQueryClient } from '@/shared/utils/get-query-client';
+
+import { filterQueryKeys } from '@/filters/core/query-keys';
+import { orgQueryKeys } from '@/orgs/core/query-keys';
+import { getFilterConfig } from '@/filters/data/get-filter-config';
+import { getOrgDetails } from '@/orgs/data/get-org-details';
+import { getOrgList } from '@/orgs/data/get-org-list';
+
+import { OrgListClientPage } from './client-page';
+
+// Force SSR on this page (needed to pick up searchParams)
+export const dynamic = 'force-dynamic';
+
+interface Props {
+  searchParams: Record<string, string>;
+}
+
+const OrgListPage = async ({ searchParams: rawSearchParams }: Props) => {
+  const queryClient = getQueryClient();
+
+  const [orgListResult] = await Promise.all([
+    // Prefetch list
+    queryClient.fetchInfiniteQuery({
+      queryKey: orgQueryKeys.list(rawSearchParams),
+      queryFn: async ({ pageParam }) => getOrgList(pageParam, rawSearchParams),
+      initialPageParam: 1,
+    }),
+    // Prefetch filter config
+    queryClient.prefetchQuery({
+      queryKey: filterQueryKeys.list(rawSearchParams, ROUTE_SECTIONS.ORGS),
+      queryFn: () => getFilterConfig(`/${ROUTE_SECTIONS.ORGS}`),
+    }),
+  ]);
+
+  // Prefetch details for each org item
+  await Promise.all(
+    orgListResult.pages
+      .flatMap((page) => page.data)
+      .map(({ orgId }) =>
+        queryClient.prefetchQuery({
+          queryKey: orgQueryKeys.details(orgId),
+          queryFn: () => getOrgDetails(orgId),
+        }),
+      ),
+  );
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <OrgListClientPage rawSearchParams={rawSearchParams} />
+    </HydrationBoundary>
+  );
+};
+
+export default OrgListPage;
