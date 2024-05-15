735f58ba9ed17b990367bbe22c3f89ea706c6fc2
John Ballesteros <johnshift.dev@gmail.com>
2024-05-15 11:53:14 +0800

    chore(app): add google to nextjs config remote-patterns

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,14 @@ const withBundleAnalyzer = bundleAnalyzer({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.google.com',
+      },
+    ],
+  },
 };
 
 export default withBundleAnalyzer(nextConfig);
