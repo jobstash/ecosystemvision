c453a311a009e7ba2c8828869ae91452da520e67
John Ballesteros <89079900+johnshift@users.noreply.github.com>
2024-05-15 08:54:34 +0800

    refactor: move shared atoms to core (#10)

--- src/shared/components/nav/use-close-nav.ts
+++ src/shared/components/nav/use-close-nav.ts
@@ -1,6 +1,6 @@
 import { useAtom } from 'jotai';
 
-import { showFullscreenNavAtom } from '@/shared/atoms';
+import { showFullscreenNavAtom } from '@/shared/core/atoms';
 
 export const useCloseNav = () => {
   const [showNav, setShowNav] = useAtom(showFullscreenNavAtom);
