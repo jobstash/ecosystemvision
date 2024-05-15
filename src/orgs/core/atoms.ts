11211aaf62963b98350f97b8b30976d9dac29a97
John Ballesteros <johnshift.dev@gmail.com>
2024-05-15 10:09:02 +0800

    feat: add use-org-list hook

--- src/orgs/core/atoms.ts
+++ src/orgs/core/atoms.ts
@@ -1,3 +1,7 @@
 import { atom } from 'jotai';
 
+import { OrgDetails } from './schemas';
+
 export const activeOrgIdAtom = atom<string | null>(null);
+export const initOrgAtom = atom<OrgDetails | null>(null);
+export const orgTotalCountAtom = atom<number | null>(null);
