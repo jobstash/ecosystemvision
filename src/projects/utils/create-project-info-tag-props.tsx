d634236472f2b795eff9b59ac13ae1f62664a30c
John Ballesteros <89079900+johnshift@users.noreply.github.com>
2024-05-20 16:38:26 +0800

    feat: add project details card component (#19)
    * feat(projects): add project-details-card link component
    
    * feat(shared): add project-related icons
    
    * feat(projects): add create-project-info-tag-props util fn
    
    * feat(projects): add create-project-audit-tag-props util fn
    
    * feat(shared): add bookmark button component
    
    * feat(shared): add share button component
    
    * chore: add nextui avatar deps
    
    * feat(shared): add chains info-tag component
    
    * feat(projects): add project-details-card component
    
    * refactor(shared0: rewrite import paths

---
+++ src/projects/utils/create-project-info-tag-props.tsx
@@ -0,0 +1,80 @@
+import { ProjectInfo, ProjectInfoTags } from '@/shared/core/schemas';
+import { EnabledTagsConfig, InfoTagProps } from '@/shared/core/types';
+import { formatNumber } from '@/shared/utils/format-number';
+import { getEnabledTagsConfig } from '@/shared/utils/get-enabled-tags-config';
+import { ActiveUsersIcon } from '@/shared/components/icons/active-users-icon';
+import { CategoryIcon } from '@/shared/components/icons/category-icon';
+import { MainnetIcon } from '@/shared/components/icons/mainnet-icon';
+import { MonthlyVolumeIcon } from '@/shared/components/icons/monthly-volume-icon';
+import { RevenueIcon } from '@/shared/components/icons/revenue-icon';
+import { TvlIcon } from '@/shared/components/icons/tvl-icon';
+
+export const createProjectInfoTagProps = (
+  projectInfoTags: ProjectInfoTags,
+  config?: EnabledTagsConfig<ProjectInfo>,
+) => {
+  const {
+    category,
+    isMainnet,
+    tvl,
+    monthlyVolume,
+    monthlyActiveUsers,
+    monthlyFees,
+    monthlyRevenue,
+  } = projectInfoTags;
+
+  const enabledTagsConfig = getEnabledTagsConfig(projectInfoTags, config);
+
+  const tags: InfoTagProps[] = [];
+
+  if (category && enabledTagsConfig.category) {
+    tags.push({
+      text: `Category: ${category}`,
+      icon: <CategoryIcon />,
+    });
+  }
+
+  if (isMainnet && enabledTagsConfig.isMainnet) {
+    tags.push({
+      text: 'Mainnet',
+      icon: <MainnetIcon />,
+    });
+  }
+
+  if (tvl && enabledTagsConfig.tvl) {
+    tags.push({
+      text: `TVL: $${formatNumber(tvl)}`,
+      icon: <TvlIcon />,
+    });
+  }
+
+  if (monthlyVolume) {
+    tags.push({
+      text: `Monthly Volume: $${formatNumber(monthlyVolume)}`,
+      icon: <MonthlyVolumeIcon />,
+    });
+  }
+
+  if (monthlyFees && enabledTagsConfig.monthlyFees) {
+    tags.push({
+      text: `Monthly Fees: $${formatNumber(monthlyFees)}`,
+      icon: <MonthlyVolumeIcon />,
+    });
+  }
+
+  if (monthlyRevenue) {
+    tags.push({
+      text: `Revenue: $${formatNumber(monthlyRevenue)}`,
+      icon: <RevenueIcon />,
+    });
+  }
+
+  if (monthlyActiveUsers) {
+    tags.push({
+      text: `Active Users: ${formatNumber(monthlyActiveUsers)}`,
+      icon: <ActiveUsersIcon />,
+    });
+  }
+
+  return tags;
+};
