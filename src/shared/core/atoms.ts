b052efd519c78410bb56a38788bd35e7a20442d3
John Ballesteros <johnshift.dev@gmail.com>
2024-05-20 13:50:35 +0800

    feat(shared): add disabled-page-scroll atom

--- src/shared/core/atoms.ts
+++ src/shared/core/atoms.ts
@@ -1,5 +1,5 @@
 import { atom } from 'jotai';
 
 export const showFullscreenNavAtom = atom<boolean>(false);
-
 export const initPathAtom = atom<string | null>(null);
+export const isDisabledPageScrollAtom = atom<boolean>(false);
