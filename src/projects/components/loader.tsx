08214e510376a9a22ab42a5ca5249ce545aae598
John Ballesteros <johnshift.dev@gmail.com>
2024-05-21 19:20:46 +0800

    feat(shared): add loader component

---
+++ src/projects/components/loader.tsx
@@ -0,0 +1,35 @@
+import { Spinner } from '@nextui-org/spinner';
+import { ClassValue } from 'clsx';
+
+import { cn } from '@/shared/utils/cn';
+
+const CENTERED_CLASSNAME = 'flex justify-center items-center';
+const FULLWIDTH_CLASSNAME = 'w-full';
+
+interface Props {
+  classNames?: {
+    wrapper: ClassValue;
+    spinner: ClassValue;
+  };
+  isCentered?: boolean;
+  isFullWidth?: boolean;
+}
+
+export const Loader = (props: Props) => {
+  const { classNames, isCentered = true, isFullWidth = true } = props;
+  return (
+    <div
+      className={cn(
+        classNames?.wrapper,
+        {
+          [CENTERED_CLASSNAME]: isCentered,
+        },
+        {
+          [FULLWIDTH_CLASSNAME]: isFullWidth,
+        },
+      )}
+    >
+      <Spinner size="sm" color="white" className={cn(classNames?.spinner)} />
+    </div>
+  );
+};
