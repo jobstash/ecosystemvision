4940dde891f98a419d724df49861e2eb61b01952
John Ballesteros <johnshift.dev@gmail.com>
2024-05-21 13:01:10 +0800

    feat(projects): add project-list component

--- src/projects/core/atoms.ts
+++ src/projects/core/atoms.ts
@@ -1,3 +1,7 @@
 import { atom } from 'jotai';
 
+import { ProjectDetails } from './schemas';
+
 export const activeProjectIdAtom = atom<string | null>(null);
+export const projectTotalCountAtom = atom<number | null>(null);
+export const initProjectAtom = atom<ProjectDetails | null>(null);
