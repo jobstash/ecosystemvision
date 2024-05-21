fe2ad604f5241d2b8c1ef43e8aefddcd10669991
John Ballesteros <89079900+johnshift@users.noreply.github.com>
2024-05-21 16:03:39 +0800

    feat: project params page (#24)
    * feat(projects): add project params page
    
    * feat(app): add project params page route
    
    * chore(app): remove debug page

---
+++ src/projects/pages/project-params-page.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+
+import { ROUTE_TABS } from '@/shared/core/constants';
+
+import { useProjectDetails } from '@/projects/hooks/use-project-details';
+
+interface Props {
+  params: {
+    id: string;
+    tab: string;
+  };
+}
+
+export const ProjectParamsPage = ({ params: { id, tab } }: Props) => {
+  const { data } = useProjectDetails(id);
+
+  if (!data) return null;
+
+  if (tab === ROUTE_TABS.SHARED.DETAILS) {
+    return <ProjectDetailsCard project={data} />;
+  }
+
+  if (tab === ROUTE_TABS.SHARED.ORG) {
+    return <OrgDetailsCard hasActions org={data.organization} />;
+  }
+
+  return null;
+};
+
+const ProjectDetailsCard = dynamic(() =>
+  import(
+    '@/projects/components/project-details-cards/project-details-card'
+  ).then((m) => m.ProjectDetailsCard),
+);
+
+const OrgDetailsCard = dynamic(() =>
+  import('@/orgs/components/org-details-card').then((m) => m.OrgDetailsCard),
+);
