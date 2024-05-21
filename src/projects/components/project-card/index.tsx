ae19c30aa2d99997c88cc1ada5032e15f6c87ecf
John Ballesteros <89079900+johnshift@users.noreply.github.com>
2024-05-21 13:04:25 +0800

    feat: project list component (#21)
    * feat(projects): add project-card component
    
    * feat(projects): add core schemas
    
    * feat(projects): add project-list fetch hook
    
    * feat(projects): add project-details fetch fn
    
    * feat(projects): add project-list component

---
+++ src/projects/components/project-card/index.tsx
@@ -0,0 +1,51 @@
+import Link from 'next/link';
+
+import { HREFS } from '@/shared/core/constants';
+import { ProjectInfo } from '@/shared/core/schemas';
+import { getLogoUrl } from '@/shared/utils/get-logo-url';
+import { CardWrapper } from '@/shared/components/card-wrapper';
+import { ChainsInfoTag } from '@/shared/components/chains-info-tag';
+import { Divider } from '@/shared/components/divider';
+import { InfoTags } from '@/shared/components/info-tags';
+import { LogoTitle } from '@/shared/components/logo-title';
+
+import { activeProjectIdAtom } from '@/projects/core/atoms';
+
+import { createProjectTags } from './create-project-tags';
+
+interface Props {
+  project: ProjectInfo;
+  isInit?: boolean;
+  filterParamsString?: string;
+}
+
+export const ProjectCard = ({ project, isInit, filterParamsString }: Props) => {
+  const { id, website, logo, name, chains } = project;
+
+  const src = getLogoUrl(website, logo);
+  const { upperTags, midTags } = createProjectTags(project);
+
+  return (
+    <CardWrapper id={id} idAtom={activeProjectIdAtom}>
+      <Link
+        href={`${HREFS.PROJECTS_PAGE}/${id}/details${filterParamsString}`}
+        scroll={false}
+        data-uuid={id}
+        data-is-init={isInit ?? undefined}
+        prefetch={true}
+        className="flex flex-col gap-3 p-6"
+      >
+        <LogoTitle src={src} name={name} />
+
+        {upperTags.length > 0 && <Divider />}
+        <InfoTags isCompact tags={upperTags} />
+
+        {midTags.length > 0 && <Divider />}
+        <InfoTags isCompact tags={midTags} />
+
+        {chains.length > 0 && <Divider />}
+        <ChainsInfoTag chains={chains} />
+      </Link>
+    </CardWrapper>
+  );
+};
---
+++ src/projects/components/project-list/index.tsx
@@ -0,0 +1,68 @@
+'use client';
+
+import { cn } from '@/shared/utils/cn';
+import { reloadPage } from '@/shared/utils/reload-page';
+import { InternalErrorResult } from '@/shared/components/internal-error-result';
+import { VirtualWrapper } from '@/shared/components/virtual-wrapper';
+
+import { ProjectCard } from '@/projects/components/project-card';
+import { useFiltersContext } from '@/filters/providers/filters-provider/context';
+
+import { useProjectList } from './use-project-list';
+
+export const ProjectList = () => {
+  const {
+    projects,
+    error,
+    inViewRef,
+    hasNextPage,
+    isSuccess,
+    isPending: isPendingList,
+  } = useProjectList();
+  const hasProjects = projects.length > 0;
+
+  const { isPendingFilters, filterSearchParams } = useFiltersContext();
+
+  const isPending = [isPendingFilters, isPendingList].includes(true);
+
+  const filterParamsString =
+    filterSearchParams.size > 0 ? `${filterSearchParams.toString()}` : '';
+
+  return (
+    <>
+      {error && <InternalErrorResult onReset={reloadPage} />}
+
+      {isPending ? (
+        <p>Card Skeleton</p>
+      ) : isSuccess && hasProjects ? (
+        <>
+          <div>
+            <p>Init project card</p>
+            <p>{JSON.stringify({ filterParamsString })}</p>
+          </div>
+
+          <VirtualWrapper count={projects.length}>
+            {(index) => (
+              <div className={cn({ 'pt-8': index > 0 })}>
+                <ProjectCard
+                  project={projects[index]}
+                  filterParamsString={filterParamsString}
+                />
+              </div>
+            )}
+          </VirtualWrapper>
+
+          {hasNextPage ? (
+            <div ref={inViewRef}>
+              <p>Card Skeleton</p>
+            </div>
+          ) : (
+            <p>TODO: No more projects UI</p>
+          )}
+        </>
+      ) : (
+        <p>Empty UI</p>
+      )}
+    </>
+  );
+};
