96cae7052fd72874d1f373e2874b6569a2ccba0d
John Ballesteros <89079900+johnshift@users.noreply.github.com>
2024-05-21 15:13:40 +0800

    feat: project details layout (#23)
    * feat(projects): add project-details fetch hook
    
    * feat(projects): add init-project-details syncer component
    
    * feat(projects): add project-details panel-header component
    
    * feat(projects): add project-tabs component
    
    * feat(projects): add project-details-layout component
    
    * refactor(projects): use alias import paths

---
+++ src/projects/components/project-details-layout.tsx
@@ -0,0 +1,37 @@
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+
+import { HREFS } from '@/shared/core/constants';
+import { getQueryClient } from '@/shared/utils/get-query-client';
+import { DetailsPanelLayout } from '@/shared/components/details-panel/layout';
+
+import { projectQueryKeys } from '@/projects/core/query-keys';
+import { getProjectDetails } from '@/projects/data/get-project-details';
+
+import { InitProjectDetailsSyncer } from './init-project-details-syncer';
+import { ProjectDetailsPanelHeader } from './project-details-panel-header';
+import { ProjectTabs } from './project-tabs';
+
+interface Props {
+  id: string;
+  children: React.ReactNode;
+}
+
+export const ProjectDetailsLayout = async ({ id, children }: Props) => {
+  const queryClient = getQueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: projectQueryKeys.details(id),
+    queryFn: () => getProjectDetails({ projectId: id }),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <DetailsPanelLayout backHref={HREFS.PROJECTS_PAGE}>
+        <InitProjectDetailsSyncer id={id} />
+        <ProjectDetailsPanelHeader id={id} />
+        <ProjectTabs id={id} />
+        {children}
+      </DetailsPanelLayout>
+    </HydrationBoundary>
+  );
+};
