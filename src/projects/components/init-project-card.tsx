cf5f91413e9acae56e90fa2ecd52c469dec9d70d
John Ballesteros <johnshift.dev@gmail.com>
2024-05-22 16:45:18 +0800

    feat(projects): add init-project-card component

---
+++ src/projects/components/init-project-card.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+
+import { Spinner } from '@nextui-org/spinner';
+import { useAtomValue } from 'jotai';
+
+import { HREFS } from '@/shared/core/constants';
+
+import { initProjectAtom } from '@/projects/core/atoms';
+import { initPathAtom } from '@/shared/core/atoms';
+
+import { ProjectCard } from './project-card';
+
+interface Props {
+  filterParamsString: string;
+}
+
+export const InitProjectCard = ({ filterParamsString }: Props) => {
+  const pathname = usePathname();
+
+  const initPath = useAtomValue(initPathAtom);
+  const initProject = useAtomValue(initProjectAtom);
+
+  // Do not render if initailly on list page
+  if (initPath === HREFS.PROJECTS_PAGE) return null;
+
+  // Do not render if on listpage and no initProject (mobile)
+  if (!initProject && pathname === HREFS.PROJECTS_PAGE) return null;
+
+  // Render init-project if set
+  if (initProject) {
+    return (
+      <ProjectCard
+        isInit
+        project={initProject}
+        filterParamsString={filterParamsString}
+      />
+    );
+  }
+
+  // Defaults to a skeleton
+  return <Spinner size="sm" color="white" />;
+};
