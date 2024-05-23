c1db881a9f75d9cec9c1bb080d47e9a8df93076c
John Ballesteros <johnshift.dev@gmail.com>
2024-05-23 16:35:01 +0800

    feat(projects): add links to project-details cta buttons

--- src/projects/components/project-details-cards/index.tsx
+++ src/projects/components/project-details-cards/index.tsx
@@ -1,6 +1,11 @@
+import { FRONTEND_URL } from '@/shared/core/envs';
 import { ProjectAllInfo } from '@/shared/core/schemas';
+import { DetailsPanelActionsWrapper } from '@/shared/components/details-panel/actions-wrapper';
+import { DetailsPanelCTA } from '@/shared/components/details-panel/cta';
 
 import { ProjectDetailsCard } from './project-details-card';
+
+const CTA_TEXT = 'Explore Project';
 
 interface Props {
   projects: ProjectAllInfo[];
@@ -12,7 +17,18 @@ export const ProjectDetailsCards = ({ projects }: Props) => {
   return (
     <div className="flex flex-col gap-6">
       {projects.map((project) => (
-        <ProjectDetailsCard key={project.id} hasActions project={project} />
+        <ProjectDetailsCard
+          key={project.id}
+          project={project}
+          actions={
+            <DetailsPanelActionsWrapper>
+              <DetailsPanelCTA
+                text={CTA_TEXT}
+                href={`${FRONTEND_URL}/projects/${project.id}/details`}
+              />
+            </DetailsPanelActionsWrapper>
+          }
+        />
       ))}
     </div>
   );
--- src/projects/components/project-details-cards/project-details-card.tsx
+++ src/projects/components/project-details-cards/project-details-card.tsx
@@ -1,9 +1,10 @@
+import React from 'react';
+
 import { ProjectAllInfo } from '@/shared/core/schemas';
 import { getLogoUrl } from '@/shared/utils/get-logo-url';
 import { ChainsInfoTag } from '@/shared/components/chains-info-tag';
 import { DetailsPanelActionsWrapper } from '@/shared/components/details-panel/actions-wrapper';
 import { DetailsPanelCardWrapper } from '@/shared/components/details-panel/card-wrapper';
-import { DetailsPanelCTA } from '@/shared/components/details-panel/cta';
 import { Divider } from '@/shared/components/divider';
 import { Heading } from '@/shared/components/heading';
 import { InfoTags } from '@/shared/components/info-tags';
@@ -16,14 +17,12 @@ import { createProjectInfoTagProps } from '@/projects/utils/create-project-info-
 
 import { ProjectDetailsCardLinks } from './links';
 
-const CTA_TEXT = 'Explore Project';
-
 interface Props {
   project: ProjectAllInfo;
-  hasActions?: boolean;
+  actions?: React.ReactNode;
 }
 
-export const ProjectDetailsCard = ({ project, hasActions }: Props) => {
+export const ProjectDetailsCard = ({ project, actions }: Props) => {
   const { name, website, logo, description, chains } = project;
 
   const src = getLogoUrl(website || '', logo);
@@ -64,13 +63,10 @@ export const ProjectDetailsCard = ({ project, hasActions }: Props) => {
 
       <ChainsInfoTag chains={chains} />
 
-      {hasActions && (
+      {actions && (
         <>
           <Divider />
-
-          <DetailsPanelActionsWrapper>
-            <DetailsPanelCTA text={CTA_TEXT} />
-          </DetailsPanelActionsWrapper>
+          {actions}
         </>
       )}
     </DetailsPanelCardWrapper>
