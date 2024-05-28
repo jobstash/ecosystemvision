daf86bcbc7dd87fe728d92b41afd1b29ff3ddcd1
John Ballesteros <johnshift.dev@gmail.com>
2024-05-28 17:48:52 +0800

    chore: setup mantine

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
+    'postcss-preset-mantine': {},
+    'postcss-simple-vars': {
+      variables: {
+        'mantine-breakpoint-xs': '20em',
+        'mantine-breakpoint-sm': '22.5em',
+        'mantine-breakpoint-md': '40em',
+        'mantine-breakpoint-lg': '80em',
+        'mantine-breakpoint-xl': '120em',
+      },
+    },
     tailwindcss: {},
   },
 };
