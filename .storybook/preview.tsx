ee0c39516596cc923335a840a0be94270cbdd4d7
John Ballesteros <johnshift.dev@gmail.com>
2024-07-23 19:35:02 +0800

    chore: setup storybook

---
+++ .storybook/preview.tsx
@@ -0,0 +1,36 @@
+import '../src/app/globals.css';
+import '@mantine/core/styles.css';
+
+import type { Preview } from '@storybook/react';
+import { NextUIProvider } from '@/shared/providers/next-ui-provider';
+
+const parameters: Preview['parameters'] = {
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/i,
+    },
+  },
+  backgrounds: {
+    default: 'default',
+    values: [{ name: 'default', value: '#141317' }],
+  },
+  nextjs: {
+    appDirectory: true,
+  },
+};
+
+const decorators: Preview['decorators'] = [
+  (Story) => (
+    <NextUIProvider>
+      <Story />
+    </NextUIProvider>
+  ),
+];
+
+const preview: Preview = {
+  parameters,
+  decorators,
+};
+
+export default preview;
--- src/shared/components/primary-button.tsx
+++ src/shared/components/primary-button.tsx
@@ -11,9 +11,9 @@ interface Props extends ButtonProps {
   };
 }
 
+const TEXT_CLASS_NAME = 'shrink-0 text-xs font-semibold sm:text-sm';
 const BUTTON_CLASS_NAME =
   'flex items-center rounded-lg bg-gradient-to-l from-[#8743FF] to-[#4136F1] p-2 px-4';
-const TEXT_CLASS_NAME = 'shrink-0 text-xs font-semibold sm:text-sm';
 
 export const PrimaryButton = ({ text, classNames, ...props }: Props) => {
   const buttonClassName = cn(BUTTON_CLASS_NAME, classNames?.button);
