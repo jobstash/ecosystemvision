7a3d29d06a39af486c14f085fa7b75f30d2083e0
John Ballesteros <johnshift.dev@gmail.com>
2024-07-31 07:47:29 +0800

    refactor: add temp grants back-button

---
+++ src/grants/components/grant-back-button.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+import { Button } from '@nextui-org/react';
+
+interface Props {
+  fallbackUrl?: string;
+}
+
+export const GrantBackButton = ({ fallbackUrl = '/' }: Props) => {
+  const { push } = useRouter();
+
+  const onClick = () => {
+    push(fallbackUrl, { scroll: true });
+  };
+
+  // TODO: JOB-676
+
+  return (
+    <Button className="w-fit" onClick={onClick}>
+      Back to Grant Programs
+    </Button>
+  );
+};
