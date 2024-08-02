81e046edb30e6b8dfaef5dddb8b5f87359b6fe29
John Ballesteros <johnshift.dev@gmail.com>
2024-08-02 16:07:59 +0800

    refactor: add draft grantee interface

--- src/grants/components/grantee-list.tsx
+++ src/grants/components/grantee-list.tsx
@@ -1,16 +1,18 @@
+import { Grantee } from '../core/types';
+
 import { GranteeItem } from './grantee-item/grantee-item';
 
 interface Props {
-  grantId: string;
+  grantees: Grantee[];
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const GranteeList = ({ grantId }: Props) => {
+const GranteeList = ({ grantees }: Props) => {
   // TODO: JOB-681
   return (
     <div className="flex flex-col gap-4">
-      {Array.from({ length: 64 }).map((_, index) => (
-        <GranteeItem key={index} id={`${index + 1}`} />
+      {grantees.map((grantee) => (
+        <GranteeItem key={grantee.id} grantee={grantee} />
       ))}
     </div>
   );
---
+++ src/grants/testutils/fake-grantee.ts
@@ -0,0 +1,13 @@
+import { Grantee } from '@/grants/core/types';
+
+export const fakeGrantee: Grantee = {
+  id: 'grantee-1',
+  name: 'Layer Zero',
+  logo: null,
+  category: 'Distributed',
+  summary:
+    'Sound was founded with the hypothesis that music fans crave a deeper experience and are willing to support their favorite artists. The fixed price of music is suffocating artists, with streaming services refusing to unlock the full value of music and failing to allow music fans to directly support artists. As a result, only 0.1% of musicians make a living wage from their art. On Sound, artists can release their music and sell digital collectibles to their fans. Listeners can discover and collect new music to prove they were an early supporter and engage with their favorite artists. For the first time, curators can create playlists and share their favorite songs and earn alongside the artists.',
+  url: 'sound.xyz',
+  lastFunding: 17_000_000,
+  fundingDate: 1637452800,
+};
