a9ffc2b00f30275b3d7e77a2387c8edcc8177963
John Ballesteros <johnshift.dev@gmail.com>
2024-08-02 16:56:23 +0800

    test: add grantee-card story

---
+++ src/grants/components/grantee-card/grantee-card.stories.tsx
@@ -0,0 +1,17 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import { fakeGrantee } from '@/grants/testutils/fake-grantee';
+
+import { GranteeCard } from './grantee-card';
+
+const meta: Meta<typeof GranteeCard> = {
+  component: GranteeCard,
+  args: {
+    grantee: fakeGrantee,
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof GranteeCard>;
+
+export const Default: Story = {};
