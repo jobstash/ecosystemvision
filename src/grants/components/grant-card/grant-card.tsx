9770e0965c0e4811ee9b67cc53d29912c4d672ec
cristiano cardelli <cristianocardelli@gmail.com, cristiano@welance.com>
2024-08-09 10:32:58 +0200

    grant card mobile

--- src/grants/components/grant-card/grant-card.tsx
+++ src/grants/components/grant-card/grant-card.tsx
@@ -38,7 +38,7 @@ const createMidItems = ({ summary, categories, type }: Grant) => [
         items={categories}
         classNames={{
           root: 'text-[#B1FFB1]',
-          text: 'border-2 border-[#B1FFB1]',
+          text: 'border border-[#B1FFB1]',
         }}
       />
     ),
@@ -50,7 +50,7 @@ const createMidItems = ({ summary, categories, type }: Grant) => [
         items={[type]}
         classNames={{
           root: 'text-[#60BCFF]',
-          text: 'border-2 border-[#60BCFF]',
+          text: 'border border-[#60BCFF]',
         }}
       />
     ),
@@ -84,20 +84,36 @@ export const GrantCard = ({ grant }: Props) => {
 
   return (
     <div
-      className="to-base-dark/20 flex items-center justify-between gap-6 rounded-b-lg bg-gradient-to-tr from-tertiary/20 p-6 text-13 lg:rounded-lg lg:bg-gradient-to-t"
+      className="to-base-dark/20 flex items-center justify-between gap-6 rounded-b-lg bg-gradient-to-t from-tertiary/20 p-6 text-13 lg:rounded-lg lg:bg-gradient-to-t"
       data-uuid={id}
       data-testid={GRANT_TEST_IDS.GRANT_CARD}
     >
       <div className="flex flex-col gap-4">
         <Title className="text-2xl font-bold lg:text-32">{name}</Title>
-        <div className="flex flex-wrap">
-          <WebLinks
-            links={{ url, discord, twitter }}
+        <div className="flex flex-wrap gap-y-4">
+          <WebLinks links={{ url, discord, twitter }} />
+          <DetailItems
+            items={topItems}
+            classNames={{ container: 'gap-x-4 gap-y-1.5', root: 'text-13' }}
           />
-          <DetailItems items={topItems} />
         </div>
-        <DetailItems items={midItems} />
-        <DetailItems items={lowerItems} />
+        <DetailItems
+          items={midItems}
+          classNames={{
+            label: 'w-full pb-2 md:pb-0 md:w-auto lg:w-full lg:pb-2',
+            root: 'w-full md:w-auto first:text-white/75 lg:first:border-0 lg:first:py-0 lg:items-start lg:first:max-w-xl lg:flex-col lg:items-start lg:max-w-64',
+            container:
+              'gap-y-4 md:gap-x-6 lg:border-y lg:grow lg:py-4 lg:border-divider/10 lg:w-full',
+          }}
+        />
+        <div className='border-t border-divider/10 pt-4'>
+          <DetailItems items={lowerItems}
+            classNames={{
+              label: 'hidden',
+              root: 'gap-x-3 [&_div_span]:bg-transparent'
+            }}
+          />
+        </div>
       </div>
 
       <div className="hidden flex-col gap-4 lg:flex">
