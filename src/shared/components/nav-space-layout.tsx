7858e42b7b7d9d6f4ac3f1ad230a1fd4d5dddc12
John Ballesteros <johnshift.dev@gmail.com>
2024-08-12 22:11:21 +0800

    refactor: move nav to own layout

--- src/shared/components/nav-space-layout.tsx
+++ src/shared/components/nav-space-layout.tsx
@@ -1,7 +1,14 @@
+import { Nav } from '@/shared/components/nav';
+
 interface Props {
   children: React.ReactNode;
 }
 
-export const NavSpaceLayout = ({ children }: Props) => {
-  return <div className="min-h-screen w-full md:pl-[212px]">{children}</div>;
+export const NavLayout = ({ children }: Props) => {
+  return (
+    <>
+      <Nav />
+      <div className="min-h-screen w-full md:pl-[212px]">{children}</div>
+    </>
+  );
 };
