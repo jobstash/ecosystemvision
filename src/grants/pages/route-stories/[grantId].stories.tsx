f5cbed722a4e1caef0315563ccc75942275b0959
John Ballesteros <johnshift.dev@gmail.com>
2024-08-13 12:41:24 +0800

    test: add /grants/[grantId] route story

--- src/grants/pages/route-stories/[grantId].stories.tsx
+++ src/grants/pages/route-stories/[grantId].stories.tsx
@@ -1,35 +1,50 @@
 import { Meta, StoryObj } from '@storybook/react';
 
+import { faker } from '@faker-js/faker';
+
 import { NavLayout } from '@/shared/components/nav-space-layout';
 
-import { MockInfiniteQueryResult } from '@/shared/testutils/misc';
+import {
+  MockInfiniteQueryResult,
+  MockQueryResult,
+} from '@/shared/testutils/misc';
 
 import { GranteeList } from '@/grants/components/grantee-list';
 
 import { fakeGrant } from '@/grants/testutils/fake-grant';
 import { fakeGrantee, fakeGrantees } from '@/grants/testutils/fake-grantee';
+import { fakeGranteeProject } from '@/grants/testutils/fake-grantee-project';
 import { mockGranteeListQuery } from '@/grants/testutils/mock-grantee-list-query';
+import { mockGranteeProjectQuery } from '@/grants/testutils/mock-grantee-project-query';
 
 import { GrantPageLayout } from '@/grants/pages/grant-page-layout';
-import { GranteeDefaultPage } from '@/grants/pages/grantee-default-page';
+import { GranteeDefaultSection } from '@/grants/pages/grantee-default-section';
+
+faker.seed(69);
 
 const grantProgram = fakeGrant();
 const grantee = fakeGrantee();
 const grantees = [grantee, ...fakeGrantees().slice(1)];
+const granteeProject = fakeGranteeProject({ id: grantee.projects[0] });
 
-const Component = () => {
-  return (
-    <NavLayout>
-      <GrantPageLayout grant={grantProgram} list={<GranteeList />}>
-        <GranteeDefaultPage grantId={grantProgram.id} />
-      </GrantPageLayout>
-    </NavLayout>
-  );
+const Component = ({ content }: { content: React.ReactNode }) => {
+  return <NavLayout>{content}</NavLayout>;
 };
 
 const meta: Meta<typeof Component> = {
   title: 'grants/routes/[grantId]',
   component: Component,
+  args: {
+    content: (
+      <GrantPageLayout grant={grantProgram} list={<GranteeList />}>
+        <GranteeDefaultSection
+          grantId={grantProgram.id}
+          grantee={grantee}
+          stats={granteeProject.tabs[0].stats}
+        />
+      </GrantPageLayout>
+    ),
+  },
   parameters: {
     nextjs: {
       navigation: {
@@ -42,6 +57,9 @@ const meta: Meta<typeof Component> = {
         mockGranteeListQuery(MockInfiniteQueryResult.SUCCESS, {
           data: grantees,
         }),
+        mockGranteeProjectQuery(MockQueryResult.SUCCESS, {
+          data: granteeProject,
+        }),
       ],
     },
   },
@@ -50,4 +68,4 @@ const meta: Meta<typeof Component> = {
 export default meta;
 type Story = StoryObj<typeof Component>;
 
-export const Default: Story = {};
+export const Success: Story = {};
