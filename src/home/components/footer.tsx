4257b5809b100d5be89643d373345797a126ceeb
cristiano cardelli <cristianocardelli@gmail.com, cristiano@welance.com>
2024-08-14 13:56:07 +0200

    footer mobile

--- src/home/components/footer.tsx
+++ src/home/components/footer.tsx
@@ -1,6 +1,10 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
+import { FarcasterIcon } from '@/home/components/icons/farcaster';
+import { TelegramIcon } from '@/home/components/icons/telegram';
+import { TwitterIcon } from '@/home/components/icons/twitter';
+
 export const Footer = () => {
   return (
     <footer className="relative max-w-[1340px] px-5 pb-20 text-white md:px-10 lg:px-7 ">
@@ -11,18 +15,24 @@ export const Footer = () => {
         width={178}
         height={26}
       />
-      <ul>
+      <ul className='flex gap-4 pb-12 pt-8 [&_svg]:w-4 [&_svg]:fill-white [&_svg]:stroke-white'>
         <li>
-          <Link href={'https://www.twitter.com'}>Twitter</Link>
+          <Link href={'https://www.twitter.com'}>
+            <TwitterIcon />
+          </Link>
         </li>
         <li>
-          <Link href={'https://www.telegram.com'}>Telegram</Link>
+          <Link href={'https://www.telegram.com'}>
+            <TelegramIcon />  
+          </Link>
         </li>
         <li>
-          <Link href={'https://www.farcaster.com'}>Farcaster</Link>
+          <Link href={'https://www.farcaster.com'}>
+            <FarcasterIcon />
+          </Link>
         </li>
       </ul>
-      <ul>
+      <ul className='flex flex-col gap-y-1 pb-12'>
         <li>
           <Link href={'/projects'}>Projects</Link>
         </li>
@@ -36,8 +46,8 @@ export const Footer = () => {
           <Link href={'/grants'}>Grants</Link>
         </li>
       </ul>
-      <div className="border-t border-white">© 2024 Ecosystem.vision</div>
-      <ul>
+      <div className="border-t border-white pb-12 pt-6 text-13">© 2024 Ecosystem.vision</div>
+      <ul className='flex flex-col gap-y-3'>
         <li>
           <Link href={'/privacy-policy'}>Privacy Policy</Link>
         </li>
