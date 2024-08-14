163e3cb022b04e81e67822c496c113749e903dce
John Ballesteros <johnshift.dev@gmail.com>
2024-08-14 18:38:21 +0800

    refactor: move components to grant page layout

--- src/grants/pages/grant-page-layout.tsx
+++ src/grants/pages/grant-page-layout.tsx
@@ -1,8 +1,47 @@
+import dynamic from 'next/dynamic';
+
 import { Divider } from '@/shared/components/divider';
 
 import { Grant } from '@/grants/core/schemas';
 import { GrantBackButton } from '@/grants/components/grant-back-button';
-import { GrantCard } from '@/grants/components/grant-card';
+
+const GrantCard = dynamic(
+  () => import('@/grants/components/grant-card').then((m) => m.GrantCard),
+  {
+    ssr: true,
+    loading: () => <p>TODO: GrantCard Skeleton</p>,
+  },
+);
+
+const GranteeCard = dynamic(
+  () => import('@/grants/components/grantee-card').then((m) => m.GranteeCard),
+  {
+    ssr: true,
+    loading: () => <p>TODO: GranteeCard Skeleton</p>,
+  },
+);
+
+const ProjectSelections = dynamic(
+  () =>
+    import('@/grants/components/project-selections').then(
+      (m) => m.ProjectSelections,
+    ),
+  {
+    ssr: true,
+    loading: () => <p>TODO: ProjectSelections Skeleton</p>,
+  },
+);
+
+const ProjectTabSelection = dynamic(
+  () =>
+    import('@/grants/components/project-tab-selections').then(
+      (m) => m.ProjectTabSelection,
+    ),
+  {
+    ssr: true,
+    loading: () => <p>TODO: ProjectTabSelection Skeleton</p>,
+  },
+);
 
 interface Props {
   grant: Grant;
@@ -10,7 +49,7 @@ interface Props {
   children: React.ReactNode;
 }
 
-export const GrantPageLayout = ({ children, list, grant }: Props) => {
+export const GrantPageLayout = ({ list, grant, children }: Props) => {
   return (
     <div className="flex flex-col gap-6 p-8">
       <GrantBackButton fallbackUrl="/grants" />
@@ -24,7 +63,12 @@ export const GrantPageLayout = ({ children, list, grant }: Props) => {
       <div className="flex gap-8">
         <div className="w-full shrink-0 lg:w-4/12">{list}</div>
 
-        {children}
+        <div className="flex w-max flex-col gap-4 lg:grow">
+          <GranteeCard />
+          <ProjectSelections />
+          <ProjectTabSelection />
+          {children}
+        </div>
       </div>
     </div>
   );
