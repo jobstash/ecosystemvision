2f1f0b17c4670fdf4efbc23ad7dd0551adfe7829
John Ballesteros <johnshift.dev@gmail.com>
2024-08-14 18:28:55 +0800

    test: add grantee-card component story

--- src/grants/components/grantee-card/grantee-card.stories.tsx
+++ src/grants/components/grantee-card/grantee-card.stories.tsx
@@ -1,18 +1,41 @@
 import { Meta, StoryObj } from '@storybook/react';
 
-import { faker } from '@faker-js/faker';
+import {
+  MockInfiniteQueryResult,
+  MockQueryResult,
+} from '@/shared/testutils/misc';
 
-import { fakeGrantee } from '@/grants/testutils/fake-grantee';
+import { GranteeCard } from '@/grants/components/grantee-card/grantee-card';
 
-import { GranteeCard } from './grantee-card';
+import { fakeGrant } from '@/grants/testutils/fake-grant';
+import { fakeGrantee, fakeGrantees } from '@/grants/testutils/fake-grantee';
+import { mockGranteeListQuery } from '@/grants/testutils/mock-grantee-list-query';
+import { mockGranteeQuery } from '@/grants/testutils/mock-grantee-query';
 
-faker.seed(69);
+const grant = fakeGrant();
+const grantee = fakeGrantee();
+const grantees = [grantee, ...fakeGrantees().slice(1)];
 
 const meta: Meta<typeof GranteeCard> = {
   title: 'grants/components/grantee-card',
   component: GranteeCard,
-  args: {
-    grantee: fakeGrantee(),
+  parameters: {
+    nextjs: {
+      navigation: {
+        pathname: `/grants/${grant.id}`,
+        segments: [['grantId', grant.id]],
+      },
+    },
+    msw: {
+      handlers: [
+        mockGranteeListQuery(MockInfiniteQueryResult.SUCCESS, {
+          data: grantees,
+        }),
+        mockGranteeQuery(MockQueryResult.SUCCESS, {
+          data: grantee,
+        }),
+      ],
+    },
   },
 };
 
