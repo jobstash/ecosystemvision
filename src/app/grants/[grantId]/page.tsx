54414dd412d50e3d82ed9597c2e7685c19ac4a9b
John Ballesteros <johnshift.dev@gmail.com>
2024-08-19 22:06:43 +0800

    refactor: temp disable grants static routes

--- src/app/grants/[grantId]/page.tsx
+++ src/app/grants/[grantId]/page.tsx
@@ -1,18 +1,18 @@
-import { MAX_LIST_SIZE } from '@/shared/core/constants';
+// import { MAX_LIST_SIZE } from '@/shared/core/constants';
 
-import { getGrantList } from '@/grants/data/get-grant-list';
+// import { getGrantList } from '@/grants/data/get-grant-list';
 
 export { GranteeDetailsSection as default } from '@/grants/pages/grantee-details-section';
 
-export const generateStaticParams = async () => {
-  const grantListResponse = await getGrantList({
-    page: 1,
-    limit: MAX_LIST_SIZE,
-  });
+// export const generateStaticParams = async () => {
+//   const grantListResponse = await getGrantList({
+//     page: 1,
+//     limit: MAX_LIST_SIZE,
+//   });
 
-  const grantIds = Array.from(
-    new Set(grantListResponse.data.map((grant) => grant.id)),
-  );
+//   const grantIds = Array.from(
+//     new Set(grantListResponse.data.map((grant) => grant.id)),
+//   );
 
-  return grantIds.map((grantId) => ({ grantId }));
-};
+//   return grantIds.map((grantId) => ({ grantId }));
+// };
