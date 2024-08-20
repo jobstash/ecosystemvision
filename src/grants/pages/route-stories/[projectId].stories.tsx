487880fa1781293cf7bf17a53860dc04f1df4c51
John Ballesteros <johnshift.dev@gmail.com>
2024-08-20 14:31:32 +0800

    test(grants): add [projectId] route stories

---
+++ src/grants/pages/route-stories/[projectId].stories.tsx
@@ -0,0 +1,197 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import { faker } from '@faker-js/faker';
+
+import { NavLayout } from '@/shared/components/nav-space-layout';
+
+import {
+  MockInfiniteQueryResult,
+  MockQueryResult,
+} from '@/shared/testutils/misc';
+
+import { GranteeList } from '@/grants/components/grantee-list';
+
+import { fakeGrant } from '@/grants/testutils/fake-grant';
+import { fakeGrantee, fakeGrantees } from '@/grants/testutils/fake-grantee';
+import { fakeGranteeProject } from '@/grants/testutils/fake-grantee-project';
+import { mockGranteeListQuery } from '@/grants/testutils/mock-grantee-list-query';
+import { mockGranteeQuery } from '@/grants/testutils/mock-grantee-query';
+
+import { GrantPageLayout } from '@/grants/pages/grant-page-layout';
+import { GranteeDetailsSection } from '@/grants/pages/grantee-details-section';
+
+faker.seed(69);
+
+const grant = fakeGrant();
+const grantId = grant.id;
+const grantee = fakeGrantee();
+const granteeId = grantee.id;
+const grantees = [grantee, ...fakeGrantees().slice(1)];
+const granteeProject = fakeGranteeProject();
+grantee.projects.push(granteeProject);
+const projectId = granteeProject.id;
+
+const Component = ({ content }: { content: React.ReactNode }) => {
+  return <NavLayout>{content}</NavLayout>;
+};
+
+const meta: Meta<typeof Component> = {
+  title: 'grants/routes/[projectId]',
+  component: Component,
+  args: {
+    content: (
+      <GrantPageLayout grant={grant} list={<GranteeList />}>
+        <GranteeDetailsSection />
+      </GrantPageLayout>
+    ),
+  },
+  parameters: {
+    nextjs: {
+      navigation: {
+        pathname: `/grants/${grantId}/grantees/${granteeId}/projects/${projectId}`,
+        segments: [
+          ['grantId', grantId],
+          ['granteeId', granteeId],
+          ['projectId', projectId],
+        ],
+      },
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Component>;
+
+export const OK: Story = {
+  parameters: {
+    msw: {
+      handlers: [
+        mockGranteeListQuery(MockInfiniteQueryResult.SUCCESS, {
+          grantId,
+          data: grantees,
+        }),
+        mockGranteeQuery(MockQueryResult.SUCCESS, {
+          grantId,
+          data: grantee,
+        }),
+      ],
+    },
+  },
+};
+
+export const LoadingGrantees: Story = {
+  parameters: {
+    msw: {
+      handlers: [
+        mockGranteeListQuery(MockInfiniteQueryResult.SUCCESS, {
+          grantId: grant.id,
+          data: grantees,
+          networkDelay: 'infinite',
+        }),
+        mockGranteeQuery(MockQueryResult.SUCCESS, {
+          grantId,
+          data: grantee,
+        }),
+      ],
+    },
+  },
+};
+
+export const LoadingGrantee: Story = {
+  parameters: {
+    msw: {
+      handlers: [
+        mockGranteeListQuery(MockInfiniteQueryResult.SUCCESS, {
+          grantId: grant.id,
+          data: grantees,
+        }),
+        mockGranteeQuery(MockQueryResult.SUCCESS, {
+          grantId,
+          data: grantee,
+          networkDelay: 'infinite',
+        }),
+      ],
+    },
+  },
+};
+
+export const ErrorGrantees: Story = {
+  parameters: {
+    msw: {
+      handlers: [
+        mockGranteeListQuery(MockInfiniteQueryResult.NETWORK_ERROR, {
+          grantId: grant.id,
+        }),
+        mockGranteeQuery(MockQueryResult.SUCCESS, {
+          grantId,
+          data: grantee,
+        }),
+      ],
+    },
+  },
+};
+
+export const ErrorGrantee: Story = {
+  parameters: {
+    msw: {
+      handlers: [
+        mockGranteeListQuery(MockInfiniteQueryResult.SUCCESS, {
+          grantId: grant.id,
+          data: grantees,
+        }),
+        mockGranteeQuery(MockQueryResult.FETCH_ERROR, {
+          grantId,
+        }),
+      ],
+    },
+  },
+};
+
+export const EmptyGrantees: Story = {
+  parameters: {
+    msw: {
+      handlers: [
+        mockGranteeListQuery(MockInfiniteQueryResult.EMPTY, {
+          grantId: grant.id,
+        }),
+        mockGranteeQuery(MockQueryResult.SUCCESS, {
+          grantId,
+          data: grantee,
+        }),
+      ],
+    },
+  },
+};
+
+export const EmptyProject: Story = {
+  parameters: {
+    msw: {
+      handlers: [
+        mockGranteeListQuery(MockInfiniteQueryResult.SUCCESS, {
+          grantId: grant.id,
+        }),
+        mockGranteeQuery(MockQueryResult.SUCCESS, {
+          grantId,
+          data: { ...grantee, projects: [] },
+        }),
+      ],
+    },
+  },
+};
+
+export const NotFoundGrantee: Story = {
+  parameters: {
+    msw: {
+      handlers: [
+        mockGranteeListQuery(MockInfiniteQueryResult.SUCCESS, {
+          grantId: grant.id,
+          data: grantees,
+        }),
+        mockGranteeQuery(MockQueryResult.NOT_FOUND, {
+          grantId,
+          data: grantee,
+        }),
+      ],
+    },
+  },
+};
