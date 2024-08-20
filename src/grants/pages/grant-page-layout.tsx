f2f50e153c09e9aacbe20924b04e56d3d8dfa5a4
cristiano cardelli <cristianocardelli@gmail.com, cristiano@welance.com>
2024-08-20 18:26:25 +0200

    grant layout

--- src/grants/pages/grant-page-layout.tsx
+++ src/grants/pages/grant-page-layout.tsx
@@ -1,7 +1,5 @@
 import dynamic from 'next/dynamic';
-import React, { useEffect } from 'react';
-
-import { Divider } from '@/shared/components/divider';
+import { useEffect, useState } from 'react';
 
 import { Grant } from '@/grants/core/schemas';
 import { GrantBackButton } from '@/grants/components/grant-back-button';
@@ -51,14 +49,27 @@ interface Props {
 }
 
 export const GrantPageLayout = ({ list, grant, children }: Props) => {
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
 
   useEffect(() => {
     const handleScroll = () => {
       const fixToTop = document.getElementById('fixToTop');
-      if (window.scrollY >= 40) {
+      const secondPin = document.getElementById('secondPin');
+      const currentScrollPos = window.scrollY;
+
+      if (currentScrollPos >= 40) {
         fixToTop?.classList.add('pinned');
-      } else {
+      } else if (currentScrollPos < prevScrollPos) {
         fixToTop?.classList.remove('pinned');
+      }
+      setPrevScrollPos(currentScrollPos);
+
+      if (secondPin) {
+        if (window.scrollY >= 220) {
+          secondPin.classList.add('second-pinned');
+        } else {
+          secondPin.classList.remove('second-pinned');
+        }
       }
     };
 
@@ -66,26 +77,23 @@ export const GrantPageLayout = ({ list, grant, children }: Props) => {
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
-  }, []);
+  }, [prevScrollPos]);
+
 
   return (
     <div className="flex flex-col gap-6 px-8 pt-[116px]">
-      <div className='fixed top-0 z-30 flex h-[116px] w-full items-center bg-[#070708] px-8'>
-       <GrantBackButton fallbackUrl="/grants" />
-      </div>
-      <div className="fixed top-[116px] z-20 w-full overflow-hidden bg-[#070708] transition-all duration-700" id='fixToTop'>
-        <GrantCard grant={grant} />
-      </div>
+        <div className='fixed top-0 z-30 flex h-[116px] w-full items-center bg-[#070708] px-8'>
+        <GrantBackButton fallbackUrl="/grants" />
+        </div>
+        <div className="fixed top-[116px] z-20 w-full overflow-hidden bg-[#070708] transition-all duration-700" id='fixToTop'>
+          <GrantCard grant={grant} />
+          <div className='py-4'>{`Grantee List of ${grant.name}`}</div>
+        </div>
+      <div className='relative top-0 z-10'>
+        <div className="flew-wrap flex gap-8 pt-[350px]">
+          <div className="relative z-20 w-[500px]" id="firstPanel">{list}</div>
 
-      <div className='relative z-10 pt-[317px]'>
-        <span>{`Grantee List of ${grant.name}`}</span>
-
-        <Divider />
-
-        <div className="flex gap-8">
-          <div className="w-full lg:w-4/12">{list}</div>
-
-          <div className="flex flex-col gap-4 lg:grow">
+          <div className="hide-scrollbar absolute left-0 z-10  ml-[540px] flex h-[calc(100vh-260px)] grow flex-col gap-4 overflow-y-auto pb-24" id="secondPin">
             {/* <GranteeCard />/ */}
             {/* <ProjectSelections /> */}
             {/* <ProjectTabSelection /> */}
