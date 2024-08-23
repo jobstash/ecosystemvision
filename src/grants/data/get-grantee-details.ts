52da5c50041309ffe3e80151b529f704ac31da3b
John Ballesteros <johnshift.dev@gmail.com>
2024-08-23 21:35:41 +0800

    refactor: comment out grantee fakers

--- src/grants/data/get-grantee-details.ts
+++ src/grants/data/get-grantee-details.ts
@@ -3,14 +3,14 @@ import { mwGET } from '@/shared/utils/mw-get';
 import { grantQueryUrls } from '@/grants/core/query-urls';
 import { granteeDtoSchema } from '@/grants/core/schemas';
 
-import { fakeGrantee } from '@/grants/testutils/fake-grantee';
+// import { fakeGrantee } from '@/grants/testutils/fake-grantee';
 
 export const getGranteeDetails = async (grantId: string, granteeId: string) => {
-  return {
-    success: true,
-    message: 'Grantee details fetched successfully',
-    data: fakeGrantee(),
-  };
+  // return {
+  //   success: true,
+  //   message: 'Grantee details fetched successfully',
+  //   data: fakeGrantee(),
+  // };
 
   return mwGET({
     url: `${grantQueryUrls.grantee(grantId, granteeId)}`,
