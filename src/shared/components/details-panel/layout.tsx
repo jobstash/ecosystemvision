52fdb49434586deb977a8cacf74e0231cc484c59
cristiano cardelli <cristianocardelli@gmail.com, cristiano@welance.com>
2024-08-28 10:19:01 +0200

    job 720

--- src/shared/components/details-panel/layout.tsx
+++ src/shared/components/details-panel/layout.tsx
@@ -6,16 +6,22 @@ import { DetailsPanelBackButton } from './back-button';
 interface Props {
   backHref: string;
   children: React.ReactNode;
+  backButton: React.ReactNode;
 }
 
 export const DetailsPanelLayout = ({ backHref, children }: Props) => {
   return (
-    <div className="hide-scrollbar fixed right-0 top-0 z-20 h-screen w-[calc((100%-264px)/2)] overflow-auto bg-[#191919]  pt-[68px] md:pt-20 lg:pt-0">
+    <div className="hide-scrollbar fixed right-0 top-0 z-20 h-screen w-screen overflow-auto bg-[#191919] pt-[68px]  md:pt-20 lg:w-[calc((100%-264px)/2)] lg:pt-0">
       <MobileHeader
         left={<DetailsPanelBackButton href={backHref} />}
         className="z-50 bg-[#191919] md:left-auto md:right-0 md:flex md:w-[calc((100%-220px))] lg:hidden"
       />
-      <div className="flex flex-col gap-4 p-5 lg:p-6">{children}</div>
+      <div className="flex flex-col gap-4 p-5 lg:p-6">
+        <div className='inline-flex w-fit cursor-pointer rounded-md border border-white/10 p-2 lg:hidden'>
+          <DetailsPanelBackButton href={backHref} />
+        </div>
+        {children}
+      </div>
 
       <PageScrollDisableSyncer shouldDisable />
     </div>
--- src/shared/components/list-page-layout.tsx
+++ src/shared/components/list-page-layout.tsx
@@ -7,7 +7,7 @@ export const ListPageLayout = ({ children, list }: Props) => {
   return (
     <>
       <div className="w-full lg:w-1/2">
-        <div className="flex flex-col gap-8 px-2 py-8 pt-24 md:px-8 md:pt-8">
+        <div className="flex flex-col gap-8 px-2 pb-8 pt-16 md:px-8 md:pt-24 lg:pt-6">
           {list}
         </div>
       </div>
