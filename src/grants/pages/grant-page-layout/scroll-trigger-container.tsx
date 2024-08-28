dbc61c6090e2e0a2e1324c6772504a243b88e796
John Ballesteros <johnshift.dev@gmail.com>
2024-08-28 13:11:46 +0800

    refactor: move grant-page-layout client logic to own client component

--- src/grants/pages/grant-page-layout/scroll-trigger-container.tsx
+++ src/grants/pages/grant-page-layout/scroll-trigger-container.tsx
@@ -1,60 +1,30 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 'use client';
 
-import dynamic from 'next/dynamic';
-import { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 
 import { useIsDesktop } from '@/shared/hooks/use-media-query';
 
-import { Grant } from '@/grants/core/schemas';
-import { GrantBackButton } from '@/grants/components/grant-back-button';
-
 gsap.registerPlugin(ScrollTrigger);
 
-const GrantCard = dynamic(
-  () => import('@/grants/components/grant-card').then((m) => m.GrantCard),
-  {
-    ssr: true,
-    loading: () => <p>TODO: GrantCard Skeleton</p>,
-  },
-);
-
 interface Props {
-  grant: Grant;
-  list: React.ReactNode;
   children: React.ReactNode;
 }
 
-// eslint-disable-next-line @typescript-eslint/ban-types
-function throttle(func: Function, limit: number) {
-  let inThrottle: boolean;
-  return function (this: any, ...args: any[]) {
-    if (!inThrottle) {
-      func.apply(this, args);
-      inThrottle = true;
-      setTimeout(() => (inThrottle = false), limit);
-    }
-  };
-}
-
-export const GrantPageLayout = ({ list, grant, children }: Props) => {
+export const ScrollTriggerContainer = ({ children }: Props) => {
   const pinRef = useRef<HTMLDivElement>(null);
-  const triggerRef = useRef<HTMLDivElement>(null);
   const contentRef = useRef<HTMLDivElement>(null); // Ref for the element to add/remove classes
 
   const isDesktop = useIsDesktop();
 
   useEffect(() => {
     const element = pinRef.current;
-    const triggerElement = triggerRef.current;
     const contentElement = contentRef.current;
 
     if (element && contentElement) {
       const scrollTriggerInstance = ScrollTrigger.create({
-        trigger: triggerElement,
         start: 'top 220px',
         endTrigger: 'html',
         end: 'bottom top',
@@ -97,22 +67,21 @@ export const GrantPageLayout = ({ list, grant, children }: Props) => {
   }, [isDesktop]);
 
   return (
-    <div className="flex flex-col gap-6 px-4 pt-[56px] md:pt-20 lg:px-0 lg:pt-0">
-      <GrantCard
-        grant={grant}
-        backButton={<GrantBackButton fallbackUrl="/grants" />}
-      />
-
-      <div className="mt-[430px] md:mt-[320px] lg:mt-[370px] lg:pr-8">
-        <div className="px-2 pb-4 text-base">Grantee List</div>
-        <div className="flex gap-8">
-          <div className="w-full shrink-0 lg:w-4/12">{list}</div>
-
-          <div ref={pinRef} className="flex w-full flex-col gap-4">
-            <div ref={isDesktop ? contentRef : undefined}>{children}</div>
-          </div>
-        </div>
-      </div>
+    <div ref={pinRef} className="flex w-full flex-col gap-4">
+      <div ref={isDesktop ? contentRef : undefined}>{children}</div>
     </div>
   );
+};
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+const throttle = (func: Function, limit: number) => {
+  let inThrottle: boolean;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  return function (this: any, ...args: any[]) {
+    if (!inThrottle) {
+      func.apply(this, args);
+      inThrottle = true;
+      setTimeout(() => (inThrottle = false), limit);
+    }
+  };
 };
