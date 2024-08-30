dbeb87dbcda29dedf3171bad4aba8984993d3ee3
cristiano cardelli <cristianocardelli@gmail.com, cristiano@welance.com>
2024-08-30 11:50:31 +0200

    add apply button to grant card full

--- src/grants/components/grant-card/full-grant-card.tsx
+++ src/grants/components/grant-card/full-grant-card.tsx
@@ -1,3 +1,4 @@
+
 import { Avatar } from '@nextui-org/react';
 
 import { cn } from '@/shared/utils/cn';
@@ -6,6 +7,7 @@ import { getLogoUrl } from '@/shared/utils/get-logo-url';
 import { GRANT_TEST_IDS } from '@/grants/core/constants';
 import { Grant } from '@/grants/core/schemas';
 import { getGrantCardData } from '@/grants/utils/get-grant-card-data';
+import { ApplyButton } from '@/grants/components/grant-list/apply-button';
 import { DetailItems } from '@/grants/components/ui/base/detail-item';
 import { Title } from '@/grants/components/ui/base/title';
 import { WebLinks } from '@/grants/components/ui/base/web-links';
@@ -86,6 +88,7 @@ export const FullGrantCard = ({ grant }: Props) => {
             name={name}
           />
         </div>
+        <ApplyButton url={url} />
       </div>
     </div>
   );
