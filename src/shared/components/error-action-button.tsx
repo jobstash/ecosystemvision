76e771e03252f44274ab14a7810bc79546bbde0a
John Ballesteros <johnshift.dev@gmail.com>
2024-09-01 17:46:30 +0800

    refactor: match not-found action button color

--- src/shared/components/error-action-button.tsx
+++ src/shared/components/error-action-button.tsx
@@ -17,7 +17,7 @@ export const ErrorActionButton = (props: Props) => {
 
   return (
     <Button
-      className="flex items-center rounded-lg bg-gradient-to-l from-[#8743FF] to-[#4136F1] p-2 px-4"
+      className="is-active flex items-center rounded-lg p-2 px-4"
       onClick={onClick}
     >
       <span className="shrink-0 text-xs font-semibold sm:text-sm">{text}</span>
