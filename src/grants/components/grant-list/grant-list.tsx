4b88873581dec31a04b4bebf24f8b3e5bd1732c6
John Ballesteros <johnshift.dev@gmail.com>
2024-11-04 18:37:48 +0800

    refactor: set inactive flag for grant-impact list

--- src/grants/components/grant-list/grant-list.tsx
+++ src/grants/components/grant-list/grant-list.tsx
@@ -5,7 +5,7 @@ import { GrantListItems } from '@/grants/components/grant-list/grant-list-items'
 import { useGrantList } from './use-grant-list';
 
 export const FetchedGrantList = () => {
-  const props = useGrantList();
+  const props = useGrantList('inactive');
   return <GrantListItems {...props} />;
 };
 
