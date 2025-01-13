ca882fe95e0eee48bfd227acecb0895b53988fb6
johnshift <johnshift.dev@gmail.com>
2025-01-13 18:28:17 +0800

    refactor: init details page

--- src/app/organizations/info/[slug]/page.tsx
+++ src/app/organizations/info/[slug]/page.tsx
@@ -1,6 +1,19 @@
-const Page = () => {
-  return (
-    <div>TODO: DETAILS PAGE</div>
-  )
+import { getOrgDetails } from '@/orgs/data/get-org-details';
+
+interface Props {
+  params: {
+    slug: string;
+  };
 }
-export default Page
+
+const Page = async ({ params: { slug } }: Props) => {
+  const data = await getOrgDetails(slug);
+
+  return (
+    <div>
+      <pre>{JSON.stringify(data, undefined, '\t')}</pre>
+    </div>
+  );
+};
+
+export default Page;
--- src/app/projects/info/[slug]/page.tsx
+++ src/app/projects/info/[slug]/page.tsx
@@ -1,6 +1,19 @@
-const Page = () => {
-  return (
-    <div>TODO: DETAILS PAGE</div>
-  )
+import { getProjectDetails } from '@/projects/data/get-project-details';
+
+interface Props {
+  params: {
+    slug: string;
+  };
 }
-export default Page
+
+const Page = async ({ params: { slug } }: Props) => {
+  const data = await getProjectDetails(slug);
+
+  return (
+    <div>
+      <pre>{JSON.stringify(data, undefined, '\t')}</pre>
+    </div>
+  );
+};
+
+export default Page;
