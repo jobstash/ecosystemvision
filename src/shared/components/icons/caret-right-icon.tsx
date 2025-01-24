6472e92339617c3cae0c9f5bfc222db43d4daa95
johnshift <johnshift.dev@gmail.com>
2025-01-24 13:18:46 +0800

    refactor: move icons to shared lib

---
+++ src/shared/components/icons/caret-right-icon.tsx
@@ -0,0 +1,24 @@
+import { cn } from '@/shared/utils/cn';
+
+export const CaretRightIcon = ({
+  className,
+  ...props
+}: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className={cn('size-4', className)}
+      {...props}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m8.25 4.5 7.5 7.5-7.5 7.5"
+      />
+    </svg>
+  );
+};
