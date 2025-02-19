8798ca282f59ff9192c56d602078294723066ec7
johnshift <johnshift.dev@gmail.com>
2025-02-19 16:30:50 +0800

    test: comment-out grant-impact layout children

--- src/grants/pages/grant-page-layout/grant-page-layout.tsx
+++ src/grants/pages/grant-page-layout/grant-page-layout.tsx
@@ -5,7 +5,7 @@ import { HREFS } from '@/shared/core/constants';
 import { Grant } from '@/grants/core/schemas';
 import { GrantBackButton } from '@/grants/components/grant-back-button';
 
-import { ScrollTriggerContainer } from '@/grants/pages/grant-page-layout/scroll-trigger-container';
+// import { ScrollTriggerContainer } from '@/grants/pages/grant-page-layout/scroll-trigger-container';
 
 const GrantCard = dynamic(
   () => import('@/grants/components/grant-card').then((m) => m.GrantCard),
@@ -21,7 +21,11 @@ interface Props {
   children: React.ReactNode;
 }
 
-export const GrantPageLayout = ({ list, grant, children }: Props) => {
+export const GrantPageLayout = ({
+  list,
+  grant,
+  // children
+}: Props) => {
   return (
     <div className="flex flex-col gap-6 px-4 pt-[56px] md:pt-20 lg:px-0 lg:pt-0">
       <GrantCard
@@ -32,7 +36,8 @@ export const GrantPageLayout = ({ list, grant, children }: Props) => {
       <div className="lg:pr-8">
         <div className="flex gap-8">
           <div className="w-full shrink-0 lg:w-4/12">{list}</div>
-          <ScrollTriggerContainer>{children}</ScrollTriggerContainer>
+          {/* <ScrollTriggerContainer>{children}</ScrollTriggerContainer> */}
+          <p>{'<ScrollTriggerContainer>{children}</ScrollTriggerContainer>'}</p>
         </div>
       </div>
     </div>
