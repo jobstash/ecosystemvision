cf32f5051868c0cae3f7e7c6d19612a11b86d06f
johnshift <johnshift.dev@gmail.com>
2025-02-19 17:32:33 +0800

    test: restore parallel routes, update react-query deps

--- src/app/impact/[grantId]/@list/page.tsx
+++ src/app/impact/[grantId]/@list/page.tsx
@@ -1,53 +1,41 @@
-const ParallelGranteeList = async () => {
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+
+import { getQueryClient } from '@/shared/utils/get-query-client';
+
+import { grantQueryKeys } from '@/grants/core/query-keys';
+import { getGranteeDetails } from '@/grants/data/get-grantee-details';
+import { getGranteeList } from '@/grants/data/get-grantee-list';
+import { GranteeList } from '@/grants/components/grantee-list';
+
+interface Props {
+  params: Promise<{ grantId: string }>;
+}
+
+const ParallelGranteeList = async ({ params }: Props) => {
+  const { grantId } = await params;
+  const queryClient = getQueryClient();
+
+  const granteeListResult = await queryClient.fetchInfiniteQuery({
+    queryKey: grantQueryKeys.grantees(grantId, ''),
+    queryFn: async ({ pageParam: page }) => getGranteeList({ page, grantId }),
+    initialPageParam: 1,
+  });
+
+  // Prefetch data for first item only (default for the page enough for seo)
+  const grantee = granteeListResult.pages.flatMap((page) => page.data).at(0);
+
+  if (grantee) {
+    await queryClient.prefetchQuery({
+      queryKey: grantQueryKeys.grantee(grantId, grantee.slug),
+      queryFn: () => getGranteeDetails(grantId, grantee.slug),
+    });
+  }
+
   return (
-    <div>
-      {/* <GranteeList /> */}
-      <p>{'<ParallelGranteeList />'}</p>
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <GranteeList />
+    </HydrationBoundary>
   );
 };
 
 export default ParallelGranteeList;
-
-// import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
-
-// import { getQueryClient } from '@/shared/utils/get-query-client';
-
-// import { grantQueryKeys } from '@/grants/core/query-keys';
-// import { getGranteeDetails } from '@/grants/data/get-grantee-details';
-// import { getGranteeList } from '@/grants/data/get-grantee-list';
-// // import { GranteeList } from '@/grants/components/grantee-list';
-
-// interface Props {
-//   params: Promise<{ grantId: string }>;
-// }
-
-// const ParallelGranteeList = async ({ params }: Props) => {
-//   const { grantId } = await params;
-//   const queryClient = getQueryClient();
-
-//   const granteeListResult = await queryClient.fetchInfiniteQuery({
-//     queryKey: grantQueryKeys.grantees(grantId, ''),
-//     queryFn: async ({ pageParam: page }) => getGranteeList({ page, grantId }),
-//     initialPageParam: 1,
-//   });
-
-//   // Prefetch data for first item only (default for the page enough for seo)
-//   const grantee = granteeListResult.pages.flatMap((page) => page.data).at(0);
-
-//   if (grantee) {
-//     await queryClient.prefetchQuery({
-//       queryKey: grantQueryKeys.grantee(grantId, grantee.slug),
-//       queryFn: () => getGranteeDetails(grantId, grantee.slug),
-//     });
-//   }
-
-//   return (
-//     <HydrationBoundary state={dehydrate(queryClient)}>
-//       {/* <GranteeList /> */}
-//       <p>{'<GranteeList />'}</p>
-//     </HydrationBoundary>
-//   );
-// };
-
-// export default ParallelGranteeList;
