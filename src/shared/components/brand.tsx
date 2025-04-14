460f8f0de8975a4828bedc8427854338cb015dab
johnshift <johnshift.dev@gmail.com>
2025-04-14 12:42:29 +0800

    feat: add veri sub to brand logo

--- src/shared/components/brand.tsx
+++ src/shared/components/brand.tsx
@@ -1,18 +1,44 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
+import { FRONTEND_URL, VERI_URL } from '@/shared/core/envs';
+
+const BRAND_NAME = 'Ecosystem Vision';
+const VERI_LABEL = 'veri.xyz';
+
 export const Brand = () => {
   return (
-    <Link href="/">
-      <div className="flex max-h-10 min-w-[174px] items-center justify-center">
+    <div className="flex h-16 items-center gap-1 px-0 md:gap-3 md:px-4">
+      <Link href={FRONTEND_URL}>
         <Image
-          src={'/ecosystem-logo.png'}
-          alt={'Ecosystem.vision'}
-          className="bg-transparent"
-          width={178}
-          height={26}
+          priority
+          width={32}
+          height={32}
+          quality={100}
+          src="/header-image.jpg"
+          alt="Ecosystem Vision Logo"
+          style={{ objectFit: 'contain' }}
         />
+      </Link>
+      <div className="hidden flex-col justify-center md:flex">
+        <Link
+          href={FRONTEND_URL}
+          className="mt-1 text-14 font-bold leading-4 md:mt-0"
+        >
+          {BRAND_NAME}
+        </Link>
+        <span className="text-10 leading-snug text-white/40 md:text-xs">
+          by
+          <Link
+            href={VERI_URL}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="pl-1 hover:underline"
+          >
+            {VERI_LABEL}
+          </Link>
+        </span>
       </div>
-    </Link>
+    </div>
   );
 };
--- src/shared/core/envs.ts
+++ src/shared/core/envs.ts
@@ -2,3 +2,4 @@ export const MW_URL = process.env.NEXT_PUBLIC_MW_URL!;
 export const FRONTEND_URL = process.env.NEXT_PUBLIC_FRONTEND_URL!;
 export const PAGE_SIZE = process.env.NEXT_PUBLIC_PAGE_SIZE!;
 export const GOOGLE_ANALYTICS_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID!;
+export const VERI_URL = process.env.NEXT_PUBLIC_VERI_URL!;
